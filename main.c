8e428c40bed91f56b25f5065de107155de098eb0
SherlockPE <fabriciolopezreyes@gmail.com>
2024-02-18 19:38:29 +0100

    Fixing the data race with a mutex

--- main.c
+++ main.c
@@ -6,20 +6,25 @@
 /*   By: fabriciolopez <fabriciolopez@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 18:11:51 by fabriciolop       #+#    #+#             */
-/*   Updated: 2024/02/18 18:49:51 by fabriciolop      ###   ########.fr       */
+/*   Updated: 2024/02/18 19:35:12 by fabriciolop      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "hilos.h"
 
 int	x;
+pthread_mutex_t	mutex;
 
 void	*routine()
 {
 	printf("Hola\n");
 
-	for (int i = 0; i < 100000; i++)
+	for (int i = 0; i < 100000000; i++)
+	{
+		pthread_mutex_lock(&mutex);
 		x++;
+		pthread_mutex_unlock(&mutex);
+	}
 	printf("End of program\n");
 	return (0);
 }
@@ -29,6 +34,7 @@ int main(void)
 {
 	pthread_t	t1;
 	pthread_t	t2;
+	pthread_mutex_init(&mutex, NULL);
 
 	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
 		return (1);
@@ -43,5 +49,6 @@ int main(void)
 		return (1);
 
 	printf("Value of X: %d\n", x);
+	pthread_mutex_destroy(&mutex);
 	return 0;
 }
