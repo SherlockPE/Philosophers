f3cbdbcfacaeb14526de7e7e27a4a1e09ce07b94
SherlockPE <fabriciolopezreyes@gmail.com>
2024-02-18 21:10:14 +0100

    returning values again, but in a thread cycle

--- return_threads.c
+++ return_threads.c
@@ -3,34 +3,49 @@
 
 void    *random_number()
 {
-    int r;
-    int *result;
+	int r;
+	int *result;
 
-    r = (rand() % 6) + 1;
+	r = (rand() % 20) + 1;
 
-    result = calloc(1, 4);
-    *result = r;
+	result = calloc(1, 4);
+	*result = r;
 
-    printf("Function direction:  %p, %d\n", result, *result);
-
-    return (result);
+	// printf("Function value and address:  %p, %d\n", result, *result);
+	return (result);
 }
 
+void	d_leaks(void)
+{
+	system("leaks philo");
+}
 
 int main(void)
 {
-    srand(time(NULL));
 
-    int *result;
-    pthread_t   hilo1;
+	atexit(d_leaks);
+	srand(time(NULL));
 
-    result = 0;
-    if (pthread_create(&hilo1, NULL, random_number, NULL) != 0)
-        return (1);
+	int *result;
+	pthread_t   hilos[5];
 
-    if (pthread_join(hilo1, (void **) &result) != 0)
-        return (1);
+	result = 0;
 
-    printf("Main direction:      %p, %d\n", result, *result);
-    return 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (pthread_create(&hilos[i], NULL, random_number, NULL) != 0)
+			return (1);
+		printf("Iniciando hilo %d\n", i);
+	}
+	for (int i = 0; i < 5; i++)
+	{
+		if (pthread_join(hilos[i], (void **) &result) != 0)
+			return (1);
+		printf("Number value and address:    %p, %d\n", result, *result);
+		free(result);
+	}
+
+
+	return 0;
 }
